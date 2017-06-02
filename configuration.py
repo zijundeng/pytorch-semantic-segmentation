5d0e58e5231237be5c92226d46fc6cb3e1c9a46e
ZijunDeng <mldzj123@gmail.com>
2017-06-02 17:38:33 +0800

    implement vanilla fcn based on vgg, res net, inceptionv3 and dense net

---
+++ configuration.py
@@ -0,0 +1,8 @@
+import os
+
+# official pretrained models
+pretrained_root = '/media/b3-542/LIBRARY/ZijunDeng/PyTorch Pretrained'
+pretrained_res152 = os.path.join(pretrained_root, 'ResNet', 'resnet152-b121ed2d.pth')
+pretrained_inception_v3 = os.path.join(pretrained_root, 'Inception', 'inception_v3_google-1a9a5a14.pth')
+pretrained_vgg19 = os.path.join(pretrained_root, 'VggNet', 'vgg19-dcbb9e9d.pth')
+pretrained_dense201 = os.path.join(pretrained_root, 'DenseNet', 'densenet201-4c113574.pth')
---
+++ models/vanilla_fcn.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+from torchvision import models
+
+from configuration import *
+
+
+class VGG(nn.Module):
+    def __init__(self, pretrained=True, num_classes=21):
+        super(VGG, self).__init__()
+        vgg = models.vgg19()
+        if pretrained:
+            vgg.load_state_dict(torch.load(pretrained_vgg19))
+        self.features = vgg.features
+        self.classifier = nn.Sequential(
+            nn.Conv2d(512, 4096, kernel_size=7),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Conv2d(4096, num_classes, kernel_size=1),
+            nn.ConvTranspose2d(num_classes, num_classes, kernel_size=32, stride=32)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+class Inception(nn.Module):
+    # it is hard to keep the output size of inception unchanged and hence inception is not convenient
+    def __init__(self, pretrained=True, num_classes=21):
+        super(Inception, self).__init__()
+        inception = models.inception_v3()
+        if pretrained:
+            inception.load_state_dict(torch.load(pretrained_inception_v3))
+        self.features = nn.Sequential(
+            inception.Conv2d_1a_3x3, inception.Conv2d_2a_3x3, inception.Conv2d_2b_3x3,
+            nn.MaxPool2d(kernel_size=3, stride=2), inception.Conv2d_3b_1x1, inception.Conv2d_4a_3x3,
+            nn.MaxPool2d(kernel_size=3, stride=2), inception.Mixed_5b, inception.Mixed_5c, inception.Mixed_5d,
+            inception.Mixed_6a, inception.Mixed_6b, inception.Mixed_6c, inception.Mixed_6d, inception.Mixed_6e,
+            inception.Mixed_7a, inception.Mixed_7b, inception.Mixed_7c
+        )
+        self.classifier = nn.Sequential(
+            nn.Conv2d(2048, num_classes, kernel_size=1),
+            nn.ConvTranspose2d(num_classes, num_classes, kernel_size=32, stride=32)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+class ResNet(nn.Module):
+    def __init__(self, pretrained=True, num_classes=21):
+        super(ResNet, self).__init__()
+        res = models.resnet152()
+        if pretrained:
+            res.load_state_dict(torch.load(pretrained_res152))
+        self.features = nn.Sequential(
+            res.conv1, res.bn1, res.relu, res.maxpool, res.layer1, res.layer2, res.layer3, res.layer4
+        )
+        self.classifier = nn.Sequential(
+            nn.Conv2d(2048, num_classes, kernel_size=1),
+            nn.ConvTranspose2d(num_classes, num_classes, kernel_size=32, stride=32)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+class DenseNet(nn.Module):
+    def __init__(self, pretrained=True, num_classes=21):
+        super(DenseNet, self).__init__()
+        dense = models.densenet201()
+        if pretrained:
+            dense.load_state_dict(torch.load(pretrained_dense201))
+        self.features = nn.Sequential(
+            dense.features,
+            nn.ReLU(inplace=True)
+        )
+        self.classifier = nn.Sequential(
+            nn.Conv2d(1920, num_classes, kernel_size=1),
+            nn.ConvTranspose2d(num_classes, num_classes, kernel_size=32, stride=32)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
